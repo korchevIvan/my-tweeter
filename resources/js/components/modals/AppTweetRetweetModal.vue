b2788a6e64aa4ac45d7d9e3bc68d875b680ab111
unbsold <37555344+unbsold@users.noreply.github.com>
2022-07-12 21:54:18 +0300

    Setup modal window

---
+++ resources/js/components/modals/AppTweetRetweetModal.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        {{ tweet }}
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        tweet: {
+            required: true,
+            type: Object
+        }
+    }
+}
+
+</script>
